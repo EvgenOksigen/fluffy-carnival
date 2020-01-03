039bfa695ebdfe96ca30dbb077bf7c28762ed6ce
EvgenOksigen <e.dedenev@gmail.com>
2020-01-04 01:30:53 +0200

    :Render list

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './ToDo/TodoList'
 
 function App() {
+  const todos = [
+    { id: 1, completed: false, title: 'Buy bread' },
+    { id: 2, completed: false, title: 'Buy onion' },
+    { id: 3, completed: false, title: 'Buy house' }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1> React tutorial</h1>
+      <TodoList todos={todos}></TodoList>
     </div>
   );
 }
